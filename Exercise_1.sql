5244d4b8cea3312bccb4cfb301dda39bf7a51782
Sam Forrester <SForrester@spartaglobal.com>
2019-09-22 22:13:20 +0100

    Tweaks to Exercise 2

--- Exercise_1.sql
+++ Exercise_1.sql
@@ -3,62 +3,76 @@ USE Northwind;
 --1.1
 --SELECT CustomerID, CompanyName, Address, City, Region, PostalCode, Country
 --FROM Customers
---WHERE City = 'Paris' OR City = 'London' --outputs only customers in Paris or London
+--WHERE City='Paris' OR City='London' 
+----Outputs only customers in Paris or London
 
 --1.2
 --SELECT ProductName 
 --FROM Products
---WHERE QuantityPerUnit LIKE '%bottles%' --outputs only products sold in bottles
+--WHERE QuantityPerUnit LIKE '%bottles%' 
+----Outputs only products sold in bottles
 
 --1.3
---SELECT ProductName, CompanyName AS 'Supplier Name', Country --Include extra column names
+--SELECT ProductName, CompanyName AS 'Supplier Name', Country 
+----Include extra column names
 --FROM Products P
---JOIN Suppliers S --Join table to suppliers to get the extra information
---ON S.SupplierID=P.SupplierID --What column do the two tables have in common
+----Join table to suppliers to get the extra information
+--JOIN Suppliers S 
+--ON S.SupplierID=P.SupplierID --What column do the two tables have in common?
 --WHERE QuantityPerUnit LIKE '%bottles%'
 
 --1.4 
 --SELECT C.CategoryName, 
 --COUNT(C.CategoryID) /*Count how many category ID's we have. These are unique so its the most effective way to do it.*/ AS 'How many in category'
 --FROM Categories C
---JOIN Products P --Join to products to get the product information
---ON P.CategoryID=C.CategoryID --Which column do they have in common
---GROUP BY CategoryName --Group the information by category, so that each category only appears once and we can use the aggregated function "Count"
---ORDER BY 'How many in category' DESC; --Order them by the most to least in each category
+--JOIN Products P 
+----Join to products to get the product information
+--ON P.CategoryID=C.CategoryID 
+----Which column do they have in common?
+--GROUP BY CategoryName 
+----Group the information by category, so that each category only appears once and we can use the aggregated function "Count"
+--ORDER BY 'How many in category' DESC; 
+----Order them by the most to least in each category
 
 --1.5
---SELECT TitleOfCourtesy + ' ' + FirstName + ' ' + LastName AS 'Employee Name', --Show The Employees' names combined with their title. This uses concatenation.
+--SELECT TitleOfCourtesy + ' ' + FirstName + ' ' + LastName AS 'Employee Name', 
+----Show The Employees' names combined with their title. This uses concatenation.
 --City AS 'City of Residence'
 --FROM Employees 
 
 --1.6
-SELECT RegionDescription, ROUND(SUM(UnitPrice*Quantity*(1-Discount)),2) AS 'Total Sales' --Show the territory and sum their total sales = unit price * quantity. Make sure to round this figure to two decimal places.
-FROM [Order Details] OD
-JOIN Orders O
-ON OD.OrderID=O.OrderID
-JOIN EmployeeTerritories ET		 
-ON ET.EmployeeID=O.EmployeeID
-JOIN Territories T 
-ON T.TerritoryID=ET.TerritoryID
-JOIN Region R --Four-fold join to connect the territory information with the order details (and hence the information on sales of products)
-ON R.RegionID = T.RegionID
-GROUP BY RegionDescription --Grouping by the Region name 
-HAVING ROUND(SUM(UnitPrice * Quantity*(1-Discount)),2) > 1000000 --Only show entries with a total sales of over 1,000,000
---Confused because I'm not getting any territories with sales over 1million??
-SELECT * FROM [Order Details]
-
+--SELECT RegionDescription AS 'Region Description', ROUND(SUM(UnitPrice*Quantity*(1-Discount)),2) AS 'Total Sales (Including Discount)' 
+----Show the territory and sum their total sales = unit price * quantity, less any discount. Make sure to round this figure to two decimal places.
+--FROM [Order Details] OD
+--JOIN Orders O
+--ON OD.OrderID=O.OrderID
+--JOIN EmployeeTerritories ET		 
+--ON ET.EmployeeID=O.EmployeeID
+--JOIN Territories T 
+--ON T.TerritoryID=ET.TerritoryID
+--JOIN Region R 
+--ON R.RegionID=T.RegionID
+----Four-fold join to connect the territory information with the order details (and hence the information on sales of products)
+--GROUP BY RegionDescription 
+----Grouping by the Region name 
+--HAVING ROUND(SUM(UnitPrice * Quantity*(1-Discount)),2) > 1000000 
+----Only show entries with a total sales of over 1,000,000
 
 --1.7
---SELECT COUNT(Distinct OrderID) AS 'How many Orders' --Count only order IDs with freight bigger than 100 and in USA or UK
+--SELECT COUNT(Distinct OrderID) AS 'How many Orders' 
+----Count only order IDs 
 --FROM Orders 
---WHERE Freight > 100.00 
---AND ShipCountry IN ('USA', 'UK')
+--WHERE Freight>100 
+----With freight bigger than 100 
+--AND ShipCountry IN ('USA','UK');
+----and in USA or UK
 
 --1.8
---SELECT TOP 1 O.OrderID, ROUND(MAX((UnitPrice * Quantity)*(1-Discount)),2) AS 'Discount Applied'
---FROM Orders O
---JOIN [Order Details] OD
---ON O.OrderID = OD.OrderID
---GROUP BY O.OrderID
---ORDER BY MAX((UnitPrice * Quantity)*(1-Discount)) DESC
+--SELECT TOP 1 OD.OrderID, ROUND(MAX((UnitPrice*Quantity)*(1-Discount)),2) AS 'Maximum Discount Applied' 
+----Select the order ID and find the maximum cost of all the different orders. 
+--FROM [Order Details] OD
+--GROUP BY OD.OrderID 
+----Group each order so it only shows each order ID once
+--ORDER BY MAX((UnitPrice*Quantity)*(1-Discount)) DESC 
+----Order the results by each group's maximum. Combined with "TOP 1" in the select part of the query and we can only show one results, the highest maximum.
 
--- Exercise_2.sql
+++ Exercise_2.sql
@@ -1,241 +1,276 @@
-USE Sparta_Global;
+--CREATE DATABASE Sparta_Global_Test;
+USE Sparta_Global_Test;
 
---CREATE TABLE CourseName
---(
---CourseID INT IDENTITY PRIMARY KEY,
---CourseName VARCHAR(15)
---)
---INSERT INTO CourseName
---(CourseName)
---VALUES ('BA-Test'), ('Engineering')
---SELECT * FROM CourseName
+CREATE TABLE CourseName 
+--Creates the CourseName Table
+(
+CourseID INT IDENTITY PRIMARY KEY, --Sets primary key (PK) on CourseID and the data type
+CourseName VARCHAR(15) --Course name column and data time
+)
+INSERT INTO CourseName
+(CourseName)
+VALUES ('BA-Test'), ('Engineering') 
+--Inserts the values of the data 
 
---CREATE TABLE StartDate
---(
---StartDateID INT IDENTITY PRIMARY KEY,
---StartDate DATETIME
---)
 
---ALTER TABLE CourseName
---ADD StartDateID INT FOREIGN KEY REFERENCES StartDate(StartDateID)
---INSERT INTO StartDate
---(StartDate)
---VALUES 
---('2018-01-15'), ('2018-01-22')
---ALTER TABLE StartDate ADD CourseID INT FOREIGN KEY REFERENCES CourseName(CourseID)
- --UPDATE CourseName
- --SET StartDateID = 1
- --WHERE CourseID = 1
- --UPDATE CourseName
- --SET StartDateID = 2
- --WHERE CourseID = 2
- --UPDATE StartDate 
- --SET CourseID =1
- --WHERE StartDateID =1
- --UPDATE StartDate 
- --SET CourseID =2
- --WHERE StartDateID =2
- --CREATE TABLE EndDate
- --(EndDateID INT IDENTITY PRIMARY KEY,
- --EndDate DATE)
- --INSERT INTO EndDate
- --(EndDate)
- --VALUES ('2018-03-02'), ('2018-03-03')
- --ALTER TABLE CourseName
- --ADD EndDateID INT
- --ALTER TABLE CourseName
- --ADD CONSTRAINT FK_EndDateID
- --FOREIGN KEY (EndDateID) REFERENCES EndDate(EndDateID)
- --INSERT INTO CourseName
- --(EndDateID)
- --VALUES (1), (2)
- --UPDATE CourseName
- --SET EndDateID = 1
- --WHERE CourseID =1
- --UPDATE CourseName
- --SET EndDateID = 2
- --WHERE CourseID =2
+CREATE TABLE StartDate
+(
+StartDateID INT IDENTITY PRIMARY KEY, 
+StartDate DATE
+)
+--More table building and data type setting including PK
+
+ALTER TABLE CourseName
+ADD StartDateID INT FOREIGN KEY REFERENCES StartDate(StartDateID) 
+--Adding a column as a foreign key (FK) in CourseName to designate the link between the tables
+INSERT INTO StartDate
+(StartDate)
+VALUES 
+('2018-01-15'), ('2018-01-22') 
+--Input values
+
+UPDATE CourseName
+SET StartDateID = 1
+WHERE CourseID = 1
+UPDATE CourseName
+SET StartDateID = 2
+WHERE CourseID = 2 
+--Input values for StartDateID in CourseName table, for linking FK
  
- --CREATE TABLE Academy
- --(AcademyID INT IDENTITY PRIMARY KEY,
- --AcademyName VARCHAR(20))
- --INSERT INTO Academy
- --(AcademyName)
- --Values
- --('Richmond')
---ALTER TABLE CourseName
---ADD AcademyID INT
---ALTER TABLE CourseName
---ADD CONSTRAINT FK_AcademyID
---FOREIGN KEY (AcademyID) REFERENCES Academy(AcademyID)
+CREATE TABLE EndDate
+(EndDateID INT IDENTITY PRIMARY KEY,
+EndDate DATE)
+INSERT INTO EndDate
+(EndDate)
+VALUES ('2018-03-02'), ('2018-03-03')
+--Create Table for end date with PK, data types and values
 
---UPDATE CourseName
---SET AcademyID = 1
---WHERE CourseID = 1 OR CourseID=2
+ALTER TABLE CourseName
+ADD EndDateID INT
+ALTER TABLE CourseName
+ADD CONSTRAINT FK_EndDateID
+FOREIGN KEY (EndDateID) REFERENCES EndDate(EndDateID)
+--Add column in CourseName for FK EndDateID and set data types
 
---CREATE TABLE RoomName
---(RoomID INT IDENTITY PRIMARY KEY,
---RoomNumber VARCHAR(15)
---)
+UPDATE CourseName
+SET EndDateID = 1
+WHERE CourseID =1
+UPDATE CourseName
+SET EndDateID = 2
+WHERE CourseID =2
+--Add the FK values in CourseName
+ 
+CREATE TABLE Academy
+(AcademyID INT IDENTITY PRIMARY KEY,
+AcademyName VARCHAR(20))
+INSERT INTO Academy
+(AcademyName)
+Values
+('Richmond')
+ALTER TABLE CourseName
+ADD AcademyID INT
+ALTER TABLE CourseName
+ADD CONSTRAINT FK_AcademyID
+FOREIGN KEY (AcademyID) REFERENCES Academy(AcademyID)
+--Creates table for Academy and adds the only value for Richmond.
+--Adds a FK for CourseName to refer to this table.
 
---INSERT INTO RoomName
---(RoomNumber)
---VALUES
---('Room 1'), ('Room 3')
+UPDATE CourseName
+SET AcademyID = 1
+WHERE CourseID = 1 OR CourseID=2
+--Input Values for FK in CourseName
 
---ALTER TABLE CourseName
---ADD RoomID INT
+CREATE TABLE RoomName
+(RoomID INT IDENTITY PRIMARY KEY,
+RoomNumber VARCHAR(15)
+)
+--Set up table for Rooms with PK
 
---ALTER TABLE CourseName
---ADD CONSTRAINT FK_RoomID
---FOREIGN KEY (RoomID) REFERENCES RoomName(RoomID)
+INSERT INTO RoomName
+(RoomNumber)
+VALUES
+('Room 1'), ('Room 3')
+--Input values
 
---UPDATE CourseName
---SET RoomID=1
---WHERE CourseID=1
---UPDATE CourseName
---SET RoomID=2
---WHERE CourseID=2
+ALTER TABLE CourseName
+ADD RoomID INT
+--Adds column for roomID
 
---CREATE TABLE Trainer
---(TrainerID INT IDENTITY PRIMARY KEY,
---TrainerName VARCHAR(30)
---)
+ALTER TABLE CourseName
+ADD CONSTRAINT FK_RoomID
+FOREIGN KEY (RoomID) REFERENCES RoomName(RoomID)
+--Set up FK for CourseName and Rooms
 
---INSERT INTO Trainer
---(TrainerName)
---VALUES
---('Tim Cawte'),('Richard Gurney')
+UPDATE CourseName
+SET RoomID=1
+WHERE CourseID=1
+UPDATE CourseName
+SET RoomID=2
+WHERE CourseID=2
+--Input the values for the FK in CourseName
 
---ALTER TABLE CourseName
---ADD TrainerID INT 
+CREATE TABLE Trainer
+(TrainerID INT IDENTITY PRIMARY KEY,
+TrainerName VARCHAR(30)
+)
+--Create table for trainers with PK
 
---ALTER TABLE CourseName
---ADD CONSTRAINT FK_TrainerID
---FOREIGN KEY (TrainerID) REFERENCES Trainer(TrainerID)
+INSERT INTO Trainer
+(TrainerName)
+VALUES
+('Tim Cawte'),('Richard Gurney')
+--Input Values
 
---UPDATE CourseName
---SET TrainerID = 1
---WHERE CourseID = 1
---UPDATE CourseName
---SET TrainerID = 2
---WHERE CourseID = 2
+ALTER TABLE CourseName
+ADD TrainerID INT 
+--Add column for trainer in Coursename
 
+ALTER TABLE CourseName
+ADD CONSTRAINT FK_TrainerID
+FOREIGN KEY (TrainerID) REFERENCES Trainer(TrainerID)
+--Give it a FK
 
---CREATE TABLE Spartans
---(SpartanID INT IDENTITY PRIMARY KEY,
---SpartanName VARCHAR(30))
+UPDATE CourseName
+SET TrainerID = 1
+WHERE CourseID = 1
+UPDATE CourseName
+SET TrainerID = 2
+WHERE CourseID = 2
+--Values
 
---INSERT INTO Spartans
---(SpartanName)
---VALUES
---('Adam Smith'),
---('John Williams'),
---('Nick Willis'),
---('Jenny Jones'),
---('Katie Prince'),
---('Peter Brown'),
---('Mo Khan'),
---('Juan Carlos'),
---('Jan Miller'),
---('Kyle Carpenter'),
---('Alvarao Carlos'),
---('Margaret Baker'),
---('Oti Mwase')
+CREATE TABLE Spartans
+(SpartanID INT IDENTITY PRIMARY KEY,
+SpartanName VARCHAR(30))
+--Table for spartans
 
---ALTER TABLE Spartans
---ADD CourseID INT 
+INSERT INTO Spartans
+(SpartanName)
+VALUES
+('Adam Smith'),
+('John Williams'),
+('Nick Willis'),
+('Jenny Jones'),
+('Katie Prince'),
+('Peter Brown'),
+('Mo Khan'),
+('Juan Carlos'),
+('Jan Miller'),
+('Kyle Carpenter'),
+('Alvarao Carlos'),
+('Margaret Baker'),
+('Oti Mwase')
+--Values
 
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 1
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 2
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 3
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 4
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 5
---UPDATE Spartans
---SET CourseID = 1 WHERE SpartanID = 6
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 7
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 8
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 9
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 10
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 11
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 12
---UPDATE Spartans
---SET CourseID = 2 WHERE SpartanID = 13
+ALTER TABLE Spartans
+ADD CourseID INT 
+--Add column for Course ID in spartans
 
---ALTER TABLE Spartans
---ADD CONSTRAINT FK_CourseID
---FOREIGN KEY (CourseID) REFERENCES CourseName(CourseID)
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 1
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 2
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 3
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 4
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 5
+UPDATE Spartans
+SET CourseID = 1 WHERE SpartanID = 6
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 7
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 8
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 9
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 10
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 11
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 12
+UPDATE Spartans
+SET CourseID = 2 WHERE SpartanID = 13
+--Add values for spartans courses. Realised I could've done this with a WHERE SpartanID >/< number next time.
 
---INSERT INTO Trainer
---(TrainerName)
---VALUES 
---('Filipe Paiva')
+ALTER TABLE Spartans
+ADD CONSTRAINT FK_CourseID
+FOREIGN KEY (CourseID) REFERENCES CourseName(CourseID)
+--Give it a FK
 
---INSERT INTO Spartans
---(SpartanName, CourseID)
---VALUES 
---('Sam Forrester', 2),
---('David Naim', 2), 
---('Abror Ilkhamov', 2), 
---('Ben Morgan', 2), 
---('Francis Thevipagan', 2), 
---('Jack Wallace', 2), 
---('Jillian Guillen', 2), 
---('Lennox Bampoe-Addo', 2), 
---('Miles Eastwood', 2), 
---('Moustafa Akanmu', 2), 
---('Rory Stokes', 2), 
---('Sharik Gurung', 2), 
---('Vinuzan Ratnasingam', 2), 
---('Vishnu Jeyarathnam', 2)
+INSERT INTO Trainer
+(TrainerName)
+VALUES 
+('Filipe Paiva')
+--Add Filipe to the trainer list
 
---INSERT INTO StartDate
---(StartDate)
---VALUES ('2019-09-09')
---INSERT INTO EndDate
---(EndDate)
---VALUES ('2019-11-15')
---INSERT INTO Academy
---(AcademyName)
---VALUES ('London Wall')
---INSERT INTO RoomName
---(RoomNumber)
---VALUES ('Room 2')
+INSERT INTO Spartans
+(SpartanName, CourseID)
+VALUES 
+('Sam Forrester', 2),
+('David Naim', 2), 
+('Abror Ilkhamov', 2), 
+('Ben Morgan', 2), 
+('Francis Thevipagan', 2), 
+('Jack Wallace', 2), 
+('Jillian Guillen', 2), 
+('Lennox Bampoe-Addo', 2), 
+('Miles Eastwood', 2), 
+('Moustafa Akanmu', 2), 
+('Rory Stokes', 2), 
+('Sharik Gurung', 2), 
+('Vinuzan Ratnasingam', 2), 
+('Vishnu Jeyarathnam', 2)
+--Add Engineering42 to the spartans list under Engineering general course
 
---INSERT INTO CourseName
---(CourseName, StartDateID, EndDateID, AcademyID, RoomID, TrainerID)
---VALUES
---('Engineering 42', 3, 3, 2, 3, 5)
+INSERT INTO StartDate
+(StartDate)
+VALUES ('2019-09-09')
+INSERT INTO EndDate
+(EndDate)
+VALUES ('2019-11-15')
+INSERT INTO Academy
+(AcademyName)
+VALUES ('London Wall')
+INSERT INTO RoomName
+(RoomNumber)
+VALUES ('Room 2')
+--Add miscellaneous values for the Engineering 42 course in support tables
 
---DELETE FROM Spartans 
---WHERE SpartanID >=14
+INSERT INTO CourseName
+(CourseName, StartDateID, EndDateID, AcademyID, RoomID, TrainerID)
+VALUES
+('Engineering 42', 3, 3, 2, 3, 3) 
+--Add miscellaneous values for the Engineering 42 course in Course table
+--We now have a course with its own ID called explicitly 'Engineering 42'
+ 
+DELETE FROM Spartans 
+WHERE SpartanID >=14 --Delete the old spartans Data (I realise now I should have used 'UPDATE' to change, rather than delete and re-add).
 
---INSERT INTO Spartans
---(SpartanName, CourseID)
---VALUES 
---('Sam Forrester', 12),
---('David Naim', 12), 
---('Abror Ilkhamov', 12), 
---('Ben Morgan', 12), 
---('Francis Thevipagan', 12), 
---('Jack Wallace', 12), 
---('Jillian Guillen', 12), 
---('Lennox Bampoe-Addo', 12), 
---('Miles Eastwood', 12), 
---('Moustafa Akanmu', 12), 
---('Rory Stokes', 12), 
---('Sharik Gurung', 12), 
---('Vinuzan Ratnasingam', 12), 
---('Vishnu Jeyarathnam', 12)
---Editing the new data so that we can separate new students from older ones, they are on a different course in a new building.
+INSERT INTO Spartans
+(SpartanName, CourseID)
+VALUES 
+('Sam Forrester', 3),
+('David Naim', 3), 
+('Abror Ilkhamov', 3), 
+('Ben Morgan', 3), 
+('Francis Thevipagan', 3), 
+('Jack Wallace', 3), 
+('Jillian Guillen', 3), 
+('Lennox Bampoe-Addo', 3), 
+('Miles Eastwood', 3), 
+('Moustafa Akanmu', 3), 
+('Rory Stokes', 3), 
+('Sharik Gurung', 3), 
+('Vinuzan Ratnasingam', 3), 
+('Vishnu Jeyarathnam', 3) --Using CourseID as '12' because that is the autoassigned key given to that course, after I made various mistakes in set up and deleted several other course entries which took up the IDs 3 to 11.
+--In re-implementation, this courseID value would likely be '3', not '12'.
+--Adding the new Spartan data so that we can separate new Engineering 42 students from older ones, they are on a different course in a new building.
+
+SELECT * FROM CourseName
+SELECT * FROM Spartans 
+SELECT * FROM Academy
+SELECT * FROM RoomName
+SELECT * FROM StartDate
+SELECT * FROM EndDate
+SELECT * FROM Trainer 
